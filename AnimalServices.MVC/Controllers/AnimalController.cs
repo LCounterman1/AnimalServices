e4f781327eba2d131a21b2380c9cc30db3b4a26b
Leslie Counterman <LCounterman1@gmail.com>
2022-02-03 15:07:10 -0500

    animal create view

--- AnimalServices.MVC/Controllers/AnimalController.cs
+++ AnimalServices.MVC/Controllers/AnimalController.cs
@@ -10,10 +10,25 @@ namespace AnimalServices.MVC.Controllers
     [Authorize]
     public class AnimalController : Controller
     {
-        // GET: Animal
         public ActionResult Index()
         {
             var model = new AnimalListItem[0];
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(AnimalCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
             return View(model);
         }
     }
